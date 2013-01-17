ca7e05c7d54c34e53a14b5f5aa1c5c08d2396bba
Wes Hunt <4thddev@gmail.com>
2013-01-16 18:02:45 -0700

    Added sample for MTP WP session

--- wp-content/themes/bsdc/front-page.php
+++ wp-content/themes/bsdc/front-page.php
@@ -10,49 +10,39 @@ get_header(); ?>
 
 <div id="sliderWraper">
     <div id="featuredSlider" class="flexslider">
+        <!-- Good spot for custom post types -->
         <ul class="slides">
+            
+            <?php
+            $query = 'post_type=slide&posts_per_page=-1';
+            $slideQuery = new WP_Query( $query );
+            if( $slideQuery->have_posts() ) : while ( $slideQuery->have_posts() ) : $slideQuery->the_post();
+                $slideImage = (object)get_field( 'slide_image' );
+            ?>
+            
             <li>
-                <img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/ny2.jpg" width="1600" height="1200" alt="" />
+                <img src="<?php echo $slideImage->url ?>" width="1600" height="1200" />
                 <div class="featured-wraper container container-twelve">
                     <div class="featured-content seven columns">
-                        <h1 class="featured-title"><a href="#">This is a featured post</a></h1>
+                        <h1 class="featured-title"><a href="#"><?php the_title(); ?></a></h1>
                         <div class="featured-entry">
-                            <p>Ut molestie dictum volutpat. Nam volutpat fermentum vestibulum. Vivamus molestie fringilla nibh non fermentum. Pellentesque sodales magna et lectus euismod consequat in sed libero.<a href="#">( Read More &rarr; )</a></p>
+                            <?php the_content(); ?>
                         </div><!-- .featured-entry-->
                     </div><!-- .featured-content -->
                 </div><!-- .featured-wraper -->
             </li>
-            <li>
-                <img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/ny3.jpg" width="1600" height="1200" alt="" />
-                <div class="featured-wraper container container-twelve">
-                    <div class="featured-content seven columns">
-                        <h1 class="featured-title"><a href="#">Another great post that wraps</a></h1>
-                        <div class="featured-entry">
-                            <p>Pellentesque sodales magna et lectus euismod consequat in sed libero. In non mollis tortor. Sed libero augue, venenatis vitae lobortis vel, lobortis id dolor. Curabitur iaculis varius elit, accumsan malesuada urna vulputate eget... <a href="#">( Read More &rarr; )</a></p>
-                        </div><!-- .featured-entry-->
-                    </div><!-- .featured-content -->
-                </div><!-- .featured-wraper -->
-            </li>
-            <li>
-                <img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/ny1.jpg" width="1600" height="1200" alt="" />
-                <div class="featured-wraper container container-twelve">
-                    <div class="featured-content seven columns">
-                        <h1 class="featured-title"><a href="#">Full width featured image</a></h1>
-                        <div class="featured-entry">
-                            <p> In non mollis tortor. Sed libero augue, venenatis vitae lobortis vel, lobortis id dolor. Curabitur iaculis varius elit, accumsan malesuada urna vulputate eget. Etiam et velit et diam congue vestibulum. Phasellus commodo... <a href="#">( Read More &rarr; )</a></p>
-                        </div><!-- .featured-entry-->
-                    </div><!-- .featured-content -->
-                </div><!-- .featured-wraper -->
-            </li>
+            
+            <?php endwhile; endif; ?>
+            
         </ul>
     </div>
 </div><!-- #sliderWraper -->
 
 <div id="twitterWraper">
     <div class="container container-twelve">
-        <div id="tweet" class="nine columns">
-        </div><!-- .nine -->
-        <div id="topSocial" class="three columns">
+<!--        <div id="tweet" class="nine columns">
+        </div> .nine -->
+        <div id="topSocial" class="twelve columns">
             <ul>
                 <li class="topTwitter"><a href="https://twitter.com/bigskydevcon">Twitter</a></li>
                 <li class="topFacebook"><a href="https://www.facebook.com/MontanaProgrammers">Facebook</a></li>
@@ -64,159 +54,35 @@ get_header(); ?>
 
 <div class="container container-twelve">
 	
-		<div id="counter-container" class="twelve columns container-box">
-			<div class="seven columns alpha">
-				<div class="container-box-inner em-large">
-					<p>Meet other techies, developers, designers, and tech-preneurs at MSU in Bozeman, MT</p>
-				</div>
-			</div>
-			<div class="five columns omega">
-				<div class="container-box-inner">
-					<div class="counter">
-					</div><!-- #counter -->
-					<div class="buy-tickets">
-						<a href="#">Buy Tickets Now</a>
-					</div>
-				</div>
-			</div>
-		</div><!-- .twelve -->
-		<div class="clear"></div>
-		<div class="box-bottom bottom-full"></div>		
-		<div id="speakers-wraper" class="container container-twelve">
-			<div class="twelve columns">
-				<div class=" container-box">
-					<div class="container-box-inner">
-						<h2 class="heading center"><span>The speakers</span></h2>
-						<div id="speakers">
-							<div id="carousel" class="flexslider">
-								<ul class="slides">
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker1.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Johnathan Doe</a><br />
-						    	    		<span class="small">Designer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker2.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Janice Doe</a><br />
-						    	    		<span class="small">Java Developer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker3.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Rebecca Diaz</a><br />
-						    	    		<span class="small">PHP Guru</span>
-						    	    	</div><!-- .speaker-desc -->									
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker4.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Juan Lienson</a><br />
-						    	    		<span class="small">Ruby Guru</span>
-						    	    	</div><!-- .speaker-desc -->										
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker5.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Mark Davison</a><br />
-						    	    		<span class="small">Python Developer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker6.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Matt Skerlson</a><br />
-						    	    		<span class="small">Designer/Developer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker1.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Johnathan Doe</a><br />
-						    	    		<span class="small">Designer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker2.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Janice Doe</a><br />
-						    	    		<span class="small">Java Developer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker3.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Rebecca Diaz</a><br />
-						    	    		<span class="small">PHP Guru</span>
-						    	    	</div><!-- .speaker-desc -->									
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker4.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Juan Lienson</a><br />
-						    	    		<span class="small">Ruby Guru</span>
-						    	    	</div><!-- .speaker-desc -->										
-									</li>
-									<li>
-										<a href="#"><img src="<?php echo get_bloginfo( 'wpurl' ) ?>/wp-content/themes/bsdc/images/placeholders/speaker5.jpg" alt="speaker2" width="150" height="150" /></a>
-										<div class="speaker-desc">
-						    	    		<a href="#">Mark Davison</a><br />
-						    	    		<span class="small">Python Developer</span>
-						    	    	</div><!-- .speaker-desc -->
-									</li>
-								</ul>
-							</div><!-- .flexslider -->
-						</div><!-- #speakers -->
-					</div><!-- .container-box.inner -->	
-				</div><!-- .container-box -->
-				<div class="box-bottom bottom-full"></div>
-			</div>
-		</div><!-- #speakers-wraper -->
-	
-		<div class="eight columns">
+		<div id="front-left-column" class="six columns">
 			<div class="container-box">
 				<div class="container-box-inner">
-					<h2 class="heading"><span>What is this event all about?</span></h2>
-						<ul class="check">
-							<li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in. Vulputate sem tristique cursus.</li>
-							<li>Magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan.</li>
-							<li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula.</li>
-							<li>Vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo.</li>
-							<li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut.</li>
-						</ul>
+                    
+                <?php if ( !dynamic_sidebar('left-column-widget-area') ) : ?>
+                    
+                    <h2 class="heading">Our default left column</h2>
+                    
+                <?php endif; ?>
+                    
 				</div><!-- .container-box-inner -->
 			</div><!-- .container-box -->
 			<div class="box-bottom bottom-main"></div>
-		</div><!-- .eight -->
+		</div><!-- .six -->
 		
-		<div class="four columns">
+		<div id="front-right-column" class="six columns">
 			<div class="container-box">
 				<div class="container-box-inner">
-					<h2 class="heading"><span>News</span></h2>
-						<ul class="calendar">
-							<li>
-								<a href="#">Lorem ipsum dolor sit amet, 
-	consectetur adipiscing elit.</a>
-								<br />
-								<span class="small">June 18th</span>
-							</li>
-							<li>
-								<a href="#">Vonsectetur adipiscing elit</a>
-								<br />
-								<span class="small">June 13th</span>
-							</li>
-							<li>
-								<a href="#">Dolor sit amet, consectetur adip</a>
-								<span class="small">May 27th</span>
-							</li>
-						</ul>
+                    
+                <?php if ( !dynamic_sidebar('right-column-widget-area') ) : ?>
+                    
+                    <h2 class="heading">Our default right column</h2>
+                        
+                <?php endif; ?>
+                        
 				</div><!-- .container-box-inner -->
 			</div><!-- .container-box -->
 			<div class="box-bottom bottom-sidebar"></div>
-		</div><!-- .four -->
+		</div><!-- .six -->
 		
 		<div class="twelve columns">
 			<div class="container-box">
--- wp-content/themes/bsdc/functions.php
+++ wp-content/themes/bsdc/functions.php
@@ -1,8 +1,8 @@
 <?php
 
-/////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 // Setup our sidebar and widgets
-
+/////////////////////////////////////////////////////////////////////////
 // register_sidebar();  If not inheriting from a parent theme you'll probably need to add this.
 
 function bsdc_widgets_init()
@@ -10,33 +10,171 @@ function bsdc_widgets_init()
     // The parent theme adds a ton of widget areas to the sidebar, we want to create some areas
     // that we can drop widgets into our child theme.
     register_sidebar( array(
-                        'name'          => __( 'Left Column Front Page', 'bsdc' ),
+                        'name'          => 'Left Column Front Page',
                         'id'            => 'left-column-widget-area',
-                        'description'   => __( 'Left column on the front page', 'bsdc' ),
-                        'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-                        'after_widget'  => '</li>',
-                        'before_title'  => '<h3 class="widget-title">',
-                        'after_title'   => '</h3>',
+                        'description'   => 'Left column on the front page',
+                        'before_widget' => '',
+                        'after_widget'  => '',
+                        'before_title'  => '<h2 class="heading">',
+                        'after_title'   => '</h2>',
     ) );
     
     register_sidebar( array(
-                        'name'          => __( 'Middle Column Front Page', 'bsdc' ),
-                        'id'            => 'middle-column-widget-area',
-                        'description'   => __( 'Middle column on the front page', 'bsdc' ),
-                        'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-                        'after_widget'  => '</li>',
-                        'before_title'  => '<h3 class="widget-title">',
-                        'after_title'   => '</h3>',
-    ) );
-    
-    register_sidebar( array(
-                        'name'          => __( 'Right Column Front Page', 'bsdc' ),
+                        'name'          => 'Right Column Front Page',
                         'id'            => 'right-column-widget-area',
-                        'description'   => __( 'Right column on the front page', 'bsdc' ),
-                        'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-                        'after_widget'  => '</li>',
-                        'before_title'  => '<h3 class="widget-title">',
-                        'after_title'   => '</h3>',
+                        'description'   => 'Right column on the front page',
+                        'before_widget' => '',
+                        'after_widget'  => '',
+                        'before_title'  => '<h2 class="heading">',
+                        'after_title'   => '</h2>',
     ) );
 }
 add_action( 'widgets_init', 'bsdc_widgets_init' );
+
+
+/////////////////////////////////////////////////////////////////////////
+// Widgets!!!
+/////////////////////////////////////////////////////////////////////////
+
+/**
+ * Adds Facebook_Widget widget.
+ * Must implement the constructor, widget, update, and form methods.
+ */
+class Facebook_Widget extends WP_Widget
+{
+
+    /**
+     * Register widget with WordPress.
+     */
+    public function __construct()
+    {
+        parent::__construct(
+                'facebook_widget', // Base ID
+                'Facebook_Widget', // Name
+                array( 'description' => 'A Widget to show a Facbook stream', ) // Args
+        );
+    }
+
+    /**
+     * Front-end display of widget.
+     *
+     * @see WP_Widget::widget()
+     *
+     * @param array $args     Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget( $args, $instance )
+    {
+        extract( $args );
+        $widgetOutput = '';
+
+        $widgetOutput .= $before_widget;
+        if( !empty( $instance['title'] ) )
+        {
+            $widgetOutput .= $before_title . $instance['title'] . $after_title;
+        }
+        
+        if( !empty( $instance['fb-account']) )
+        {
+            $widgetOutput .= '<div class="fb-like-box" data-href="https://www.facebook.com/' . $instance['fb-account'] . '" data-width="400" data-show-faces="true" data-stream="true" data-header="true"></div>';
+        
+            // Lazy - could add this to wp_footer
+            $widgetOutput .= '<div id="fb-root"></div>
+                                <script>(function(d, s, id) {
+                                  var js, fjs = d.getElementsByTagName(s)[0];
+                                  if (d.getElementById(id)) return;
+                                  js = d.createElement(s); js.id = id;
+                                  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+                                  fjs.parentNode.insertBefore(js, fjs);
+                                }(document, "script", "facebook-jssdk"));</script>';
+        }
+        $widgetOutput .= $after_widget;
+        
+        echo $widgetOutput;
+    }
+
+    /**
+     * Sanitize widget form values as they are saved.
+     *
+     * @see WP_Widget::update()
+     *
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     *
+     * @return array Updated safe values to be saved.
+     */
+    public function update( $new_instance, $old_instance )
+    {
+        $instance = array( );
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['fb-account'] = strip_tags( $new_instance['fb-account'] );
+
+        return $instance;
+    }
+
+    /**
+     * Back-end widget form.
+     *
+     * @see WP_Widget::form()
+     *
+     * @param array $instance Previously saved values from database.
+     */
+    public function form( $instance )
+    {
+        if( isset( $instance['title'] ) )
+        {
+            $title = $instance['title'];
+        }
+        else
+        {
+            $title = 'Facebook';
+        }
+        
+        if( isset( $instance['fb-account']) )
+        {
+            $fbAccount = $instance['fb-account'];
+        }
+        else
+        {
+            $fbAccount = '';
+        }
+        ?>
+        		<p>
+        		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php echo 'Title:'; ?></label> 
+        		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        		</p>
+                
+                <p>
+        		<label for="<?php echo $this->get_field_id( 'fb-account' ); ?>"><?php echo 'Facebook Account:'; ?></label> 
+        		<input class="widefat" id="<?php echo $this->get_field_id( 'fb-account' ); ?>" name="<?php echo $this->get_field_name( 'fb-account' ); ?>" type="text" value="<?php echo esc_attr( $fbAccount ); ?>" />
+        		</p>
+        <?php
+    }
+
+}
+// register Foo_Widget widget
+add_action( 'widgets_init', create_function( '', 'register_widget( "facebook_widget" );' ) );
+
+
+
+/////////////////////////////////////////////////////////////////////////
+// Post Types
+/////////////////////////////////////////////////////////////////////////
+
+/**
+ * We need to tell WP to add a post type to the defaults
+ */
+function create_post_type()
+{
+    register_post_type( 'slide',
+                        array(
+                                'labels'        => array(
+                                'name'          => __( 'Slides' ),
+                                'singular_name' => __( 'Slide' )
+                        ),
+                        'public'        => true,
+                        'has_archive'   => false
+                        )           
+    );
+}
+add_action( 'init', 'create_post_type' );
